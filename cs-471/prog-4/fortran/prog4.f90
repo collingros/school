85df39464a20e07d3e796502df33d4929f7fd9b3
Collin Gros <vexprof@gmail.com>
2020-09-28 13:43:05 -0600

    actually i forgot about input, added now

---
+++ cs-471/prog-4/fortran/prog4.f90
@@ -0,0 +1,12 @@
+! collin gros
+! 09-28-2828
+! cs471
+! program #4
+!
+!
+! this program performs gauss elimination with backwards substitution
+! on a randomly generated matrix with a user-given size,
+! and reports how long it takes to complete the gaussian elimination/subst
+! part.
+!
+
