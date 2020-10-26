4dc1924c5c7dde491f222a1ee29afa2181f6f27f
Collin Gros <vexprof@gmail.com>
2020-10-26 21:31:02 +0000

    finished the little assignment

--- cs-471/ch9-js/418.js
+++ cs-471/ch9-js/418.js
@@ -1,3 +1,20 @@
+// collin gros
+// 10-26-2020
+// cs471
+//
+// problem description:
+	// Implement the code on page 394 n JavaScript.  (page 418 in Version 10 of
+	// the book)
+	// page 418 11th Global Edition
+	// Determine if JavaScript implements Shallow, Deep, of Ad Hoc Binding.
+//
+// code was copied from the textbook - pg 418 (11th global).
+// this shows that javascript uses "deep binding" because the var 'x' after
+// the call to sub4 with param sub2 (in sub2) is 1. this is what sub2
+// has seen all along, which means javascript looked at sub2's environment,
+// meaning deep binding is used.
+//
+
 function sub1()
 {
 	console.log("sub1");
@@ -6,6 +23,9 @@ function sub1()
 	{
 		console.log("sub2");
 		//alert(x); // Creates a dialog box with the value of x
+		console.log ("x in sub2 is: ");
+		// javscript uses deep binding because x's value in sub2's environment
+		// is 1, and this command prints 1
 		console.log(x);
 	};
 
@@ -14,6 +34,8 @@ function sub1()
 		console.log("sub3");
 		var x;
 		x = 3;
+		console.log("x = 3 in sub3");
+		console.log("calling sub4 with params sub2");
 		sub4(sub2);
 	};
 
@@ -22,10 +44,13 @@ function sub1()
 		console.log("sub4");
 		var x;
 		x = 4;
+		console.log("x = 4 in sub4");
+		console.log("calling ...sub[x]");
 		subx();
 	};
 
 	x = 1;
+	console.log("x = 1 in sub1");
 	sub3();
 };
 
