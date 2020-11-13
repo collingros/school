18472df791d3452a82fb09f7a39f412b17288daf
Collin Gros <vexprof@gmail.com>
2020-11-13 16:58:18 -0700

    cs471-concurrency, a lot done so far just saving my work. found a mistake in .gitignore.

--- cs-471/concurrency/Concurrency.java
+++ cs-471/concurrency/Concurrency.java
@@ -11,35 +11,65 @@
 */
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 
 class Concurrency
 {
-	private static ArrayList<Thread> arrThreads = new ArrayList<Thread>();
-
 	// we use static variables to help us connect the threads
 	// to a common block
-	public static int N=0;
+	private static ArrayList<Thread> arrThreads = new ArrayList<Thread>();
+
+	// N is the size of the NxN matrix of random numbers, and the
+	// number of threads created
+	public static int N = 0;
+
+	// A stores random numbers to analyze
 	public static int[][] A;
+
+	// stats stores statistics for each row analyzed; written to by threads
+	// each entry is an integer array in the following format:
+	// 	max, min, sum, avg
+	public static int[][] stats;
+
 
 	public static void main(String[] args)
 	{
 		try {
-			// take 1 input from the user (dimension of the square matrix [N])
+			// take 1 input from the user:
+			// 	(dimension of the square matrix [N])
 
 			// create the array from input
-			int size = Integer.parseInt(args[0]);
+			N = Integer.parseInt(args[0]);
 			// create 2D NxN INTEGER matrix
-			A = new int[size][size];
+			A = new int[N][N];
+			// create stats matrix (N x 4)
+			stats = new int[N][4];
 	
 
 			// randomly assign INTEGER values to each element
-			
+			for (int i = 0; i < N; ++i) {
+				for (int j = 0; j < N; ++j) {
+					// in the range of  between 2^(32-N) and 2^(31-N)
+					// trunc(  (2^(32-N) - (2^31-N))* Random()   + 2^(31-N))
+					double tmp = Math.pow(2, 32-N)
+									- Math.pow(2, 31-N);
+					int x = (int) (Math.pow(2, 31-N)
+								+ (Math.random() * tmp));
+					A[i][j] = x;
+				}
+			}
 
 		// use only double/long
 		// start timer
+		long startTime = System.nanoTime();
 
 		// create N threads (each is responsible for one row of the matrix)
+		for (int i = 0; i < N; ++i) {
+			Thread t = new Thread(new ThreadTest(i));
+			t.start();
+			arrThreads.add(t);
+		}
 
 		// each thread calculates max, min, and stats for summation and avg
 		// and stores them in shared set of arrays (global variable)
@@ -49,6 +79,8 @@ class Concurrency
 		// calculate the overall max, min, and average
 
 		// stop timer
+		long stopTime = System.nanoTime();
+		long duration = stopTime - startTime;
 
 		// use only int and float for max, sum, avg.
 		// calculate the max value, min value, average of all values
---
+++ cs-471/concurrency/ThreadTest.java
@@ -0,0 +1,71 @@
+/*
+	collin gros
+	11-11-2020
+	cs471
+	concurrency
+
+	this class is used by the Concurrency class to assign threads a row
+	and analyze it for statistics reported later in the Concurrency main()
+	function.
+*/
+
+class ThreadTest implements Runnable
+{
+	int row;
+
+	ThreadTest(int row)
+	{
+		// we are analyzing row #x
+		this.row = row; 
+	}
+
+
+	public void run()
+	{
+		// initialize maximum to first element in row
+		int max = Concurrency.A[row][0];
+		// initialize minimum to first element in row
+		int min = Concurrency.A[row][0];
+		// initialize sum to 0
+		int sum = 0;
+
+		// analyze row #row
+		// loop thu row; Concurrency.N = length/width of row
+		for (int i = 0; i < Concurrency.N; ++i) {
+			System.out.println("\t"+row+":\tA["+row+"]["+i+"]"
+								+ ": " + Concurrency.A[row][i]);
+			int val = Concurrency.A[row][i];
+			// get min, max, sum
+			if (max < val) {
+				max = val;
+			}
+			if (min > val) {
+				min = val;
+			}
+
+			sum += val;
+		}
+		// calculate avg with sum and N
+		int avg = sum / Concurrency.N;
+
+
+		// store values in an integer array, which is then stored
+		// in the shared array Concurrency.stats
+		int [] stats = {max, min, sum, avg};
+		for (int i = 0; i < 4; ++i) {
+			System.out.println(row+":\t stats["+i+"]: "+stats[i]);
+		}
+
+		try{
+		Concurrency.stats[row] = stats;
+		}
+		catch (Exception e) {
+			System.out.print(e);
+			return;
+		}
+//		System.out.println(row+":\t max: "+max+" min: "+min+" sum: "+sum
+//								+" avg: "+avg);
+	}
+}
+
+
